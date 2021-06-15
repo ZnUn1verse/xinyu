80afde18c8035692a9fea94520ca7aaa070f38c0
zhangxinyu <984270000@qq.com>
2021-06-15 18:18:52 +0800

    每日一题69. x 的平方根recode

--- src/leetcode/dailypractice/No69.java
+++ src/leetcode/dailypractice/No69.java
@@ -5,28 +5,24 @@ package leetcode.dailypractice;
  * @date 2021/6/15
  */
 public class No69 {
-    public int mySqrt(int x) {
-        if (x == 0) {
-            return 0;
-        }
-        if (x == 1) {
-            return 1;
-        }
-        int l = 1;
-        int r = x / 2;
-        // 在区间 [l,r] 查找目标元素
+    public static int mySqrt(int x) {
+        int l = 0, r = x;
         while (l < r) {
-            // 避免int溢出，改用
+//            int mid = (l + r + 1) >> 1;
             int mid = l + (r - l) / 2;
-            // 避免乘法溢出，改用除法
-            if (mid > x / l) {
-                // 下一轮搜索区间是 [l,mid - 1]
-                l = mid - 1;
-            } else {
-                // 下一轮搜索区间是 [mid,r]
+            if (mid < x / mid)
+                l = mid + 1;
+            else
                 r = mid;
-            }
         }
         return l;
+    }
+
+    public static int mySqrt2(int x) {
+        return (int) Math.sqrt(x);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(No69.mySqrt2(8));
     }
 }
